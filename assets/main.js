d73c8eee069c73aa2d1daa3e34ef15369492f3c6
Andrew Dalba <andrewdalba@gmail.com>
2020-07-08 16:03:52 -0400

    update weather in all areas depending on location

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,12 @@
 var apiKey = "f2db8e1b3757f3e4e3db807ed339605e";
-var date = "7/7/2020";
+var date = moment().format("MMMM Do");
+var currentDate = "7/7/2020";
 var city = "new York";
+
+for(var i = 0; i < 5; i++){
+    // $(`#date${i+1}`).text(moment().add(i, 'days').calendar());
+    $(`#date${i+1}`).text(moment().add(i, 'days').format("dddd, (MMMM Do)"));
+}
 
 $("#searchBtn").on("click", function(){
     city = $("#citySearch").val();
@@ -19,7 +25,7 @@ function updateCurrentWeather() {
             console.log(response);
             $("#city").text(response.city.name + " (" + date + ")");
             $("#currentTemperature").text("Temperature (F): " + response.list[0].main.temp.toFixed(2));
-            $("#currentHumidity").text("Humidity: " + response.list[0].main.humidity);
+            $("#currentHumidity").text("Humidity: " + response.list[0].main.humidity + "%");
             $("#currentWindSpeed").text("Wind Speed: " + response.list[0].wind.speed);
             $("#currentWeatherIcon").attr("src", `https://openweathermap.org/img/w/${response.list[0].weather[0].icon}.png`);
         });
@@ -35,9 +41,10 @@ function updateFiveDayForecast(){
         for(var i = 0; i < 5; i++){
             var temperature = response.list[i].main.temp.toFixed(2);
             var humidity = response.list[i].main.humidity
-            $(`#temp${i+1}`).text("Temperature (F): " + temperature);
+            var icon = `https://openweathermap.org/img/w/${response.list[i].weather[0].icon}.png`
+            $(`#temp${i+1}`).text("Temp (F): " + temperature);
             $(`#humidity${i+1}`).text("Humidity: " + humidity + "%");
-
+            $(`#icon${i+1}`).attr("src", icon);
         }
 
     });
